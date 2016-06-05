88b43125f2f4ca6c2b349cf3b4c42a039d12f12c
KamiSama <1457434343@qq.com>
2016-06-05 08:45:00 +0000

    update

--- controller/data.js
+++ controller/data.js
@@ -4,10 +4,9 @@ module.exports = function(app) {
     app.get(['/data'],function(req,res) {
         ckLogin(req,res,function(t,uname) {
             if (t) {
-                // sql.all('select * from urls where isDelete=0;',function(err,rows){
-                //     res.render('pages/url.html',{layout: 'pages/layout.html',msg: null,hasErr: false,userName: uname,urls: rows});
-                // });
-                res.render('pages/data.html',{layout: 'pages/layout.html',msg: null,hasErr: false,userName: uname});
+                sql.all('select * from datas where isDelete=0;',function(err,rows){
+                    res.render('pages/data.html',{layout: 'pages/layout.html',msg: null,hasErr: false,userName: uname,datas: rows});
+                });
             } else {
                 res.redirect('/login');
             }
