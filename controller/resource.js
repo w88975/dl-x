9abf3615d9f6ea7e9c9d27884bd653ba9a807b8c
kamisama <1457434343@qq.com>
2016-06-07 00:10:42 +0800

    update

--- controller/resource.js
+++ controller/resource.js
@@ -1,10 +1,23 @@
 var cwd = process.cwd();
 var path = require('path');
+var fs = require('fs');
 module.exports = function(app) {
     app.get(/^(.+)$/, function(req, res) {
-        res.sendFile( path.join(cwd,'/views/') + req.params[0]);
+        fs.exists(path.join(cwd,'/views/') + req.params[0], function(exists) {
+          if (exists) {
+              res.sendFile( path.join(cwd,'/views/') + req.params[0]);
+          } else {
+              res.end();
+          }
+        });
     });
     app.post(/^(.+)$/, function(req, res) {
-        res.sendFile( path.join(cwd,'/views/') + req.params[0]);
+        fs.exists(path.join(cwd,'/views/') + req.params[0], function(exists) {
+          if (exists) {
+              res.sendFile( path.join(cwd,'/views/') + req.params[0]);
+          } else {
+              res.end();
+          }
+        });
     });
 };
