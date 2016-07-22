712389d3e774c7dfef2528c23cdf81a28a804f9c
codesama <1457434343@qq.com>
2016-07-22 12:49:12 +0800

    update

--- controller/login.js
+++ controller/login.js
@@ -13,6 +13,7 @@ module.exports = function(app) {
             if (rows.length >0 ) {
                 res.clearCookie('oid');
                 res.cookie('oid', rows[0].id);
+                res.cookie('isAdmin',rows[0].isAdmin);
                 return res.redirect('/admin');
             } else {
                 return res.render('pages/login.html',{layout: null,msg:'账号或密码错误!'});
