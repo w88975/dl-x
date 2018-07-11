27c9a5d423dcac5ac9bbd3d81eda2cbf67597492
KamiSama <1457434343@qq.com>
2018-07-12 00:29:18 +0800

    update

---
+++ 0k.js
@@ -0,0 +1,36 @@
+var rep = { // 替换用的数据，使用了4个零宽字符，数据量减少了一半。
+    '00': '\u200b',
+    '01': '\u200c',
+    '10': '\u200d',
+    '11': '\uFEFF'
+};
+
+function tohanzi(data)
+{
+    if(data == '') return '请输入十六进制unicode';
+    data = data.split("\\u");
+    var str ='';
+    for(var i=0;i<data.length;i++)
+    {
+        str+=String.fromCharCode(parseInt(data[i],16).toString(10));
+    }
+    return str;
+}
+
+function tounicode(data)
+{
+   if(data == '') return '请输入汉字';
+   var str =''; 
+   for(var i=0;i<data.length;i++)
+   {
+      str+="\\u"+parseInt(data[i].charCodeAt(0),10).toString(16);
+   }
+   var arr = str.split('\\u')
+   var newStr = ''
+   for (let index = 1; index < arr.length; index++) {
+        newStr+= '\\u200b'+('\\u'+arr[index])
+   }
+   return newStr;
+}
+
+console.log(tohanzi(tounicode('未登录，请先登录')))
